d41ae0bf9aa0ae92127c79881374d69634661f73
Milind_77 <milinddhote77@gmail.com>
2021-06-30 00:51:44 +0530

    [milind] Add:- created base project

---
+++ Employee_Wage_Computation_/EmployeeWage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_Wage_Computation_
+{
+    class EmployeeWage
+    {
+        public void EmpPresent()
+        {
+            //variable(constant)
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computing
+            int empcheck = random.Next(0, 2);
+            if (empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent.");
+            }
+        }
+    }
+}
---
+++ Employee_Wage_Computation_/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Employee_Wage_Computation_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<----Welcome to Employee Wage Computation---->");
+            Console.WriteLine("----------------------------------------------");
+            EmployeeWage emp = new EmployeeWage();
+            emp.EmpPresent();
+        }
+    }
+}
