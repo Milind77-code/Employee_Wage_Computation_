c4826e829a37bf89335655f2dc620ddd57f33930
Milind_77 <milinddhote77@gmail.com>
2021-06-30 01:48:26 +0530

    [milind] Add:- Cal. Max Days or Hours For a Month

--- Employee_Wage_Computation_/EmployeeWage.cs
+++ Employee_Wage_Computation_/EmployeeWage.cs
@@ -10,17 +10,18 @@ namespace Employee_Wage_Computation_
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 2;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HOURS_IN_MONTH = 100;
 
         public void EmpPresent()
         {
             //variable
             int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
-
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            int totalEmpHours = 0;
+            int totalWorkingDays = 0;
+            while (totalEmpHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 //Computing
                 int empcheck = random.Next(0, 3);
@@ -41,10 +42,10 @@ namespace Employee_Wage_Computation_
                         Console.WriteLine("when Employee is Absent");
                         break;
                 }
-                totalEmpWage += empWage;
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                Console.WriteLine("Employee Wage :- " + empWage);
+                totalEmpHours += empHrs;
+                Console.WriteLine("Day:- " + totalWorkingDays + "  Emp Working Hours :- " + empHrs);
             }
+            int totalEmpWage = totalEmpHours * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Employee Wage :- " + totalEmpWage);
         }
     }
