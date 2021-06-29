5d5e74e6faee6802e95a92edf085967f026002b6
Milind_77 <milinddhote77@gmail.com>
2021-06-30 01:36:00 +0530

    [milind] Add:- Employee Wage Using Case Statement

--- Employee_Wage_Computation_/EmployeeWage.cs
+++ Employee_Wage_Computation_/EmployeeWage.cs
@@ -6,13 +6,12 @@ namespace Employee_Wage_Computation_
 {
     class EmployeeWage
     {
+        //(constant)
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         public void EmpPresent()
         {
-            //(constant)
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
-
             //variable
             int empHrs = 0;
             int empWage = 0;
@@ -20,21 +19,23 @@ namespace Employee_Wage_Computation_
 
             //Computing
             int empcheck = random.Next(0, 3);
-            if (empcheck == IS_PART_TIME)
+            switch (empcheck)
             {
-                empHrs = 4;
-                Console.WriteLine("Employee is Working Part Time.");
-            }
-            else if (empcheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-                Console.WriteLine("Employee is Working Full Time.");
-            }
-            else
-            {
-                empHrs = 0;
-                Console.WriteLine("Employee is Absent");
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    Console.WriteLine("Employee is Working Part Time.");
+                    break;
 
+
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    Console.WriteLine("Employee is Working Full Time.");
+                    break;
+
+                default:
+                    empHrs = 0;
+                    Console.WriteLine("Employee is Absent");
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee Wage :- " + empWage);
